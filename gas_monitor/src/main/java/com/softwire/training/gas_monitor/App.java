47f9a0bfa1f0e7c3b67a3acdb26a46583cb6a2f9
Jed Johnson <jed.johnson@softwire.com>
2019-12-10 13:44:01 +0000

    Included dependencies for logging

--- gas_monitor/src/main/java/com/softwire/training/gas_monitor/App.java
+++ gas_monitor/src/main/java/com/softwire/training/gas_monitor/App.java
@@ -1,13 +1,23 @@
 package com.softwire.training.gas_monitor;
 
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static Logger logger = Logger.getLogger(App.class);
+
+    public static void main(String[] args) throws Exception {
+        //Log in console in and log file
+        logger.info("Log4j appender configuration is successful !!");
+
+        S3BucketFetcher s3Fetcher = new S3BucketFetcher();
+
+        MonitorLocation[] locations = s3Fetcher.fetchFile();
 
     }
 }
--- gas_monitor/src/main/java/com/softwire/training/gas_monitor/S3BucketFetcher.java
+++ gas_monitor/src/main/java/com/softwire/training/gas_monitor/S3BucketFetcher.java
@@ -1,48 +1,42 @@
 package com.softwire.training.gas_monitor;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 public class S3BucketFetcher {
 
-    public void S3BucketFetcher() {
+    private static Logger LOGGER = Logger.getLogger(S3BucketFetcher.class);
+
+
+    public MonitorLocation[] fetchFile() throws Exception {
         String bucket_name = "eventprocessing-locationss3bucket-7mbrb9iiisk4";
         String key_name = "locations.json";
 
-        System.out.format("Downloading %s from S3 bucket %s...\n", key_name, bucket_name);
+        LOGGER.info(String.format("Downloading %s from S3 bucket %s...\n", key_name, bucket_name));
+
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_2).build();
-        try {
-            S3Object o = s3.getObject(bucket_name, key_name);
-            S3ObjectInputStream s3is = o.getObjectContent();
-            FileOutputStream fos = new FileOutputStream(new File(key_name));
-            byte[] read_buf = new byte[1024];
-            int read_len = 0;
-            while ((read_len = s3is.read(read_buf)) > 0) {
-                fos.write(read_buf, 0, read_len);
-            }
-            s3is.close();
-            fos.close();
-        } catch (
-                AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
-            System.exit(1);
-        } catch (
-                FileNotFoundException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        } catch (
-                IOException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
+
+        S3Object s3Object = s3.getObject(bucket_name, key_name);
+        S3ObjectInputStream inputStream = s3Object.getObjectContent();
+
+        ObjectMapper mapper = new ObjectMapper();
+        MonitorLocation[] locations = mapper.readValue(inputStream, MonitorLocation[].class);
+        LOGGER.info(String.format("Received %d locations from S3 Bucket...\n", locations.length));
+
+        AmazonSNS amazonSNS = new AmazonSNSClient();
+        AmazonSQS amazonSQS = new AmazonSQSClient();
+
+
+        return locations;
     }
 }
