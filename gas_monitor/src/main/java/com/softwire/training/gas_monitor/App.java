2f06e90b0065186fd0637b27e93d2c1e573eb7db
Jed1920 <56831145+Jed1920@users.noreply.github.com>
2019-12-10 11:59:19 +0000

    Add files via upload

---
+++ gas_monitor/src/main/java/com/softwire/training/gas_monitor/App.java
@@ -0,0 +1,13 @@
+package com.softwire.training.gas_monitor;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
